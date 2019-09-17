0c05141576c9f20a062095e878de2fff87ab5bf1
sitarnet <sitarnet@ukr.net>
2019-09-17 23:21:10 +0300

    Create admin menu, acl, controller.

---
+++ app/code/Sitarnet/Vendor/Controller/Adminhtml/Vendor/Index.php
@@ -0,0 +1,45 @@
+<?php
+namespace Sitarnet\Vendor\Controller\Adminhtml\Vendor;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    const ADMIN_RESOURCE = 'Sitarnet_Vendor::vendor';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Sitarnet_Vendor::vendor');
+        $resultPage->addBreadcrumb(__('Vendors'), __('Vendors'));
+        $resultPage->addBreadcrumb(__('Manage Vendors'), __('Manage Vendors'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Vendor'));
+
+        return $resultPage;
+    }
+}
